0cb2760d8302a82b39c49323f9ab342d43dc48b4
olup <loup.topalian@gmail.com>
2023-08-13 19:00:09 +0200

    feat: aide Brave

--- src/components/UnavailableApp.tsx
+++ src/components/UnavailableApp.tsx
@@ -1,11 +1,42 @@
-import { Badge, Container, Text } from "@mantine/core";
+import { Alert, Badge, Box, Center, Code, Text } from "@mantine/core";
+import { useUserAgent } from "@oieduardorabelo/use-user-agent";
+import { IconInfoCircle } from "@tabler/icons-react";
 
 export const UnavailableApp = () => {
+  const details = useUserAgent();
+
+  console.log(details);
   return (
-    <Container>
-      <Badge mb={20}>{__COMMIT_HASH__}</Badge>
-      <Text>Votre navigateur n'est pas compatible avec cette app.</Text>
-      <Text>Essayez avec la dernière versio de Chrome, sur desktop.</Text>
-    </Container>
+    <Center h={500}>
+      <Box>
+        <Badge mb={20}>{__COMMIT_HASH__}</Badge>
+        <Text>Votre navigateur n'est pas compatible avec cette app.</Text>
+        <Text>
+          Essayez avec la dernière versio de Chrome, Edge, Brave ou Opera sur
+          desktop.
+        </Text>
+        {details?.browser?.name === "Chrome" && (
+          <Alert mt={20} icon={<IconInfoCircle />}>
+            <Text>
+              Vous utilisez <b>Brave Browser</b> ?
+            </Text>
+            <Text>
+              Vous pouvez utiliser Lunii Admin Web en activant{" "}
+              <b>Filesystem Access Api</b> dans les Flags Brave :
+            </Text>
+            <Text>
+              <ul>
+                <li>
+                  Tappez <Code>brave://flags/#file-system-access-api</Code> dans
+                  votre barre d'url
+                </li>
+                <li>Activez le flag</li>
+                <li>Rechargez Brave</li>
+              </ul>
+            </Text>
+          </Alert>
+        )}
+      </Box>
+    </Center>
   );
 };
