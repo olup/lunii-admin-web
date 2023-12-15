8f5f71904f6c31c53347d8c2a5a9f9a2660658ff
loup topalian <loup.topalian@gmail.com>
2023-12-15 11:17:38 +0100

    chore

--- src/components/UnConnectedApp.tsx
+++ src/components/UnConnectedApp.tsx
@@ -2,7 +2,7 @@ import { Alert, Badge, Button, Center, Container, Text } from "@mantine/core";
 import { getLuniiHandle } from "../utils";
 import { getDeviceInfo } from "../utils/lunii/deviceInfo";
 import { state } from "../store";
-import { IconAlertCircle } from "@tabler/icons-react";
+import { IconFolderOpen } from "@tabler/icons-react";
 
 export const UnconnectedApp = () => {
   return (
@@ -16,6 +16,7 @@ export const UnconnectedApp = () => {
 
         <Button
           mb="xl"
+          leftIcon={<IconFolderOpen size="1rem" />}
           onClick={async () => {
             const handle = await getLuniiHandle();
             if (!handle) return;
@@ -29,10 +30,8 @@ export const UnconnectedApp = () => {
           Ouvrir ma Lunii
         </Button>
         <Alert my="lg" radius="md" p="xl">
-          <Text mb="md">Un outil par Olup</Text>
-          <Text mb="sm">
-            Rendu possible grace au travail extraordinaire de{" "}
-          </Text>
+          <Text mb="md">🔨 Créé par Olup</Text>
+          <Text mb="sm">Rendu possible grace au travail incroyable de </Text>
           <Text>🥇 R. Daneel Olivaw</Text>
           <Text>🥇 Frederir</Text>
           <Text mb="md"> ❤️ Et tout le support de la communauté Lunii</Text>
